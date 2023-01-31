9466df08857e776f0d0bdf71aae46c3b489f37db
chaudharijanhavi20 <cm.a.20.janhavi.chaudhari@gmail.com>
2023-02-01 00:28:26 +0530

    done vercel setting

--- facerecog/facerecog/settings.py
+++ facerecog/facerecog/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-2ebdq$)i=h+ytzu^38a5xhr%pam=3eu9+vf^kvy3pt*i*17k_p
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [".vercel.app",".now.sh"]
 
 
 # Application definition
@@ -135,3 +136,6 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
 
 ]
+
+STATICFILES_DIRS=os.path.join(BASE_DIR,"static")
+STATIC_ROOT=os.path.join(BASE_DIR,"staticfiles_build","static")
